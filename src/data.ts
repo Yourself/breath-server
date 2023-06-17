a8dedd210a348fe9f515021f86345ab75f503363
Yourself <kyle.smith314@gmail.com>
2023-06-16 22:55:56 -0600

    add prettier more data interface

--- src/data.ts
+++ src/data.ts
@@ -1,8 +1,39 @@
-import Database from 'better-sqlite3';
-import fs from 'fs';
-import path from 'path';
+import Database from "better-sqlite3";
+import fs from "fs";
+import path from "path";
 
-export interface AirQualityData {
+export interface SensorValue {
+  rco2?: number;
+  pm02?: number;
+  tvoc?: number;
+  nox?: number;
+  atmp?: number;
+  rhum?: number;
+}
+
+interface SensorReading extends SensorValue {
+  id: string;
+}
+
+export interface SensorMetadata {
+  id: string;
+  name?: string;
+  has_rco2: boolean;
+  has_pm02: boolean;
+  has_tvoc: boolean;
+  has_nox: boolean;
+  has_atmp: boolean;
+  has_rhum: boolean;
+}
+
+export interface QueryParams {
+  start?: Date;
+  end?: Date;
+  device?: string | string[];
+}
+
+interface AQLogRow {
+  time: string;
   id: string;
   rco2?: number;
   pm02?: number;
@@ -12,98 +43,126 @@ export interface AirQualityData {
   rhum?: number;
 }
 
-export interface SensorData {
-  id: string;
-  name: string;
-  rco2: boolean;
-  pm02: boolean;
-  tvoc: boolean;
-  nox: boolean;
-  atmp: boolean;
-  rhum: boolean;
+export interface SensorTimePoint extends SensorValue {
+  time: Date;
 }
 
-export interface QueryParams {
-  start?: Date;
-  end?: Date;
-  device?: string | string[];
+export interface SensorTimeSeries {
+  id: string;
+  series: SensorTimePoint[];
 }
 
 export class AirQualityDB {
   private readonly _db: Database.Database;
 
-  private readonly insertAQ: Database.Statement<AirQualityData>;
+  private readonly insertAQ: Database.Statement<SensorReading>;
 
   private readonly insertSensor: Database.Statement<{ id: string }>;
 
   private readonly devices: Database.Statement<[]>;
 
+  private readonly updateDevice: Database.Statement<SensorMetadata>;
+
   constructor() {
-    this._db = new Database(path.join(__dirname, '..', 'data', 'data.db'));
-    this.insertAQ = this._db.prepare<AirQualityData>(`
+    this._db = new Database(path.join(__dirname, "..", "data", "data.db"));
+    this.insertAQ = this._db.prepare<SensorReading>(`
       INSERT INTO air_quality_log (id, rco2, pm02, tvoc, nox, atmp, rhum)
            VALUES ($id, $rco2, $pm02, $tvoc, $nox, $atmp, $rhum)`);
     this.insertSensor = this._db.prepare<{ id: string }>(`
       INSERT INTO sensors (id)
            SELECT $id WHERE NOT EXISTS (SELECT * FROM sensors WHERE id = $id)`);
-    this.devices = this._db.prepare('SELECT * FROM sensors');
+    this.devices = this._db.prepare("SELECT * FROM sensors");
+    this.updateDevice = this._db.prepare(`
+      UPDATE sensors
+      SET name = $name,
+          has_rco2 = $has_rco2,
+          has_pm02 = $has_pm02,
+          has_tvoc = $has_tvoc,
+          has_nox = $has_nox,
+          has_atmp = $has_atmp,
+          has_rhum = $has_rhum
+      WHERE id = $id`);
   }
 
   initTables() {
-    this._db.exec(fs.readFileSync(path.join(__dirname, '..', 'schema.sql')).toString());
+    this._db.exec(
+      fs.readFileSync(path.join(__dirname, "..", "schema.sql")).toString()
+    );
   }
 
-  insertAirQuality(quality: AirQualityData) {
+  insertAirQuality(id: string, quality: SensorValue) {
     this._db.transaction(() => {
-      this.insertSensor.run(quality);
-      this.insertAQ.run(quality);
+      this.insertSensor.run({ id });
+      this.insertAQ.run({ id, ...quality });
     });
   }
 
   getDevices() {
-    return this.devices.all() as string[];
+    return this.devices.all() as SensorMetadata[];
   }
 
-  getAQData(query: QueryParams) {
+  updateDeviceMetadata(metadata: SensorMetadata) {
+    this.updateDevice.run(metadata);
+  }
+
+  getReadings(query: QueryParams): SensorTimeSeries[] {
     const conditions = [];
     const values = [];
 
     if (query.start == null && query.end == null) {
-      conditions.push('time BETWEEN DATEADD(day, -1, GETDATE()) AND GETDATE()');
+      conditions.push("time BETWEEN DATEADD(day, -1, GETDATE()) AND GETDATE()");
     } else if (query.start == null) {
-      conditions.push('time BETWEEN DATEADD(day, -1, ?) AND ?');
+      conditions.push("time BETWEEN DATEADD(day, -1, ?) AND ?");
       values.push(query.end, query.end);
     } else if (query.end == null) {
-      conditions.push('time BETWEEN ? AND GETDATE()');
+      conditions.push("time BETWEEN ? AND GETDATE()");
       values.push(query.start);
     } else {
-      conditions.push('time BETWEEN ? AND ?');
+      conditions.push("time BETWEEN ? AND ?");
       values.push(query.start, query.end);
     }
 
     if (Array.isArray(query.device)) {
-      conditions.push(`id IN (${query.device.map((_) => '?').join(', ')})`);
+      conditions.push(`id IN (${query.device.map((_) => "?").join(", ")})`);
       values.push(...query.device);
     } else if (query.device != null) {
-      conditions.push('id = ?');
+      conditions.push("id = ?");
       values.push(query.device);
     }
 
-    const allConditions = conditions.map((c) => `(${c})`).join(' AND ');
+    const allConditions = conditions.map((c) => `(${c})`).join(" AND ");
     const sql = `SELECT * FROM air_quality_log WHERE ${allConditions}`;
-    const rows = this._db.prepare(sql).all(...values);
+    const rows = this._db.prepare(sql).all(...values) as AQLogRow[];
+    const seriesById = new Map<string, SensorTimePoint[]>();
+    for (const row of rows) {
+      const time = new Date(row.time);
+      const { rco2, pm02, tvoc, nox, atmp, rhum } = row;
+      const point = { time, rco2, pm02, tvoc, nox, atmp, rhum };
+      let series = seriesById.get(row.id);
+      if (series == null) {
+        series = [];
+        seriesById.set(row.id, series);
+      }
+      series.push(point);
+    }
+
+    const result = [];
+    for (const [id, series] of seriesById.entries()) {
+      result.push({ id, series });
+    }
+    return result;
   }
 }
 
-export function hasAQData(obj: NonNullable<unknown>): obj is AirQualityData {
-  if (!('id' in obj)) return false;
+export function hasAQData(obj: NonNullable<unknown>): obj is SensorReading {
+  if (!("id" in obj)) return false;
 
-  if ('rco2' in obj) return true;
-  if ('pm02' in obj) return true;
-  if ('tvoc' in obj) return true;
-  if ('nox' in obj) return true;
-  if ('atmp' in obj) return true;
-  if ('rhum' in obj) return true;
+  if ("rco2" in obj) return true;
+  if ("pm02" in obj) return true;
+  if ("tvoc" in obj) return true;
+  if ("nox" in obj) return true;
+  if ("atmp" in obj) return true;
+  if ("rhum" in obj) return true;
 
   return false;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import path from 'path';
-import { AirQualityDB, hasAQData } from './data';
+import express from "express";
+import path from "path";
+import { AirQualityDB, hasAQData } from "./data";
 
 const LISTEN_PORT = 3000;
 
@@ -10,26 +10,25 @@ db.initTables();
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'dist')));
+app.use(express.static(path.join(__dirname, "..", "dist")));
 app.use(express.json());
 
-app.get('/', (_, res) => {
-  res.send('Breath Server');
-})
+app.get("/", (_, res) => {
+  res.send("Breath Server");
+});
 
-app.post('/submit/:device', (req, res) => {
-  const data = { ...req.body, id: req.params.device };
-  if (hasAQData(data)) {
-    db.insertAirQuality(data);
+app.post("/submit/:device", (req, res) => {
+  if (hasAQData(req.body)) {
+    db.insertAirQuality(req.params.device, req.body);
     res.sendStatus(200);
   } else {
     res.sendStatus(400);
   }
 });
 
-app.get('/query', (req, res) => { });
+app.get("/query", (req, res) => {});
 
 app.listen(LISTEN_PORT, () => {
-  // eslint:disable-next-line:no-console
+  // eslint-disable-next-line no-console
   console.log(`Server started on port ${LISTEN_PORT}.`);
 });
